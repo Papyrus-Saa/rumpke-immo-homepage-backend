26f9bc1241065fb2a9419ffe44cc0669a7606992
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-01-09 12:49:12 +0100

    style: format code for consistency in migration file

--- src/database/migrations/1670000000002-make-title-not-null.ts
+++ src/database/migrations/1670000000002-make-title-not-null.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MakeTitleNotNull1670000000002 implements MigrationInterface {
-    name = 'MakeTitleNotNull1670000000002'
+  name = 'MakeTitleNotNull1670000000002'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`UPDATE "properties" SET "title" = '' WHERE "title" IS NULL`);
-        await queryRunner.query(`ALTER TABLE "properties" ALTER COLUMN "title" SET NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`UPDATE "properties" SET "title" = '' WHERE "title" IS NULL`);
+    await queryRunner.query(`ALTER TABLE "properties" ALTER COLUMN "title" SET NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "properties" ALTER COLUMN "title" DROP NOT NULL`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "properties" ALTER COLUMN "title" DROP NOT NULL`);
+  }
 }
